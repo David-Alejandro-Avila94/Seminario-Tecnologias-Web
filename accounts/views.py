4de033bd50cd0f0614478175c212106a8b22e7c4
David-Alejandro-Avila94 <aviladavidalejandro94@gmail.com>
2025-10-16 23:39:03 +0000

    django primera parte

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+""" esto lo cree yo """
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # redirect to home page
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
